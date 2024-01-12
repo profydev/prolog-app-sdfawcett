a3f3a501f35e116b066b2b0cf33787d0e5b32443
Shayne Fawcett <shayne.fawcett@gmail.com>
2024-01-12 17:31:42 -0500

    added storybook button component that supports all required sizes, all required colors, all required button states, and supports buttons rendered with or without icons

---
+++ features/ui/button/button.stories.tsx
@@ -0,0 +1,112 @@
+import React from "react";
+import Image from "next/image";
+import { Meta, StoryFn } from "@storybook/react";
+import { Button, ButtonSize, ButtonColor, ButtonIcon } from "./button";
+
+export default {
+  title: "UI/Button",
+  component: Button,
+  parameters: {
+    layout: "fullscreen",
+  },
+} as Meta<typeof Button>;
+
+const Template: StoryFn<typeof Button> = ({ size, color, icon, children }) => (
+  <div style={{ padding: 10 }}>
+    <Button
+      color={color}
+      size={size}
+      icon={icon}
+      onClick={() => alert("clicked")}
+    >
+      {children}
+    </Button>
+  </div>
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  size: ButtonSize.sm,
+  color: ButtonColor.primary,
+  children: "Label",
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  size: ButtonSize.sm,
+  color: ButtonColor.secondary,
+  children: "Label",
+};
+
+export const Gray = Template.bind({});
+Gray.args = {
+  size: ButtonSize.sm,
+  color: ButtonColor.gray,
+  children: "Label",
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+  size: ButtonSize.sm,
+  color: ButtonColor.empty,
+  children: "Label",
+};
+
+export const EmptyGray = Template.bind({});
+EmptyGray.args = {
+  size: ButtonSize.sm,
+  color: ButtonColor.emptyGray,
+  children: "Label",
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  size: ButtonSize.sm,
+  color: ButtonColor.error,
+  children: "Label",
+};
+
+export const EmptyError = Template.bind({});
+EmptyError.args = {
+  size: ButtonSize.sm,
+  color: ButtonColor.emptyError,
+  children: "Label",
+};
+
+export const IconOnly = Template.bind({});
+IconOnly.args = {
+  size: ButtonSize.sm,
+  color: ButtonColor.primary,
+  icon: ButtonIcon.only,
+  children: (
+    <>
+      <Image src="icons/circle.svg" alt="circle icon" width={20} height={20} />
+    </>
+  ),
+};
+
+export const IconLeading = Template.bind({});
+IconLeading.args = {
+  size: ButtonSize.sm,
+  color: ButtonColor.primary,
+  icon: ButtonIcon.leading,
+  children: (
+    <>
+      <p>Button CTA</p>
+      <Image src="icons/circle.svg" alt="circle icon" width={20} height={20} />
+    </>
+  ),
+};
+
+export const IconTrailing = Template.bind({});
+IconTrailing.args = {
+  size: ButtonSize.sm,
+  color: ButtonColor.primary,
+  icon: ButtonIcon.trailing,
+  children: (
+    <>
+      <p>Button CTA</p>
+      <Image src="icons/circle.svg" alt="circle icon" width={20} height={20} />
+    </>
+  ),
+};

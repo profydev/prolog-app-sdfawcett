137f331354a8fd8cd230089334f423c11312c0a0
Shayne Fawcett <shayne.fawcett@gmail.com>
2024-01-16 11:23:31 -0500

    fixing side nav bug where support icon rotates on sidebar toggle only the collapse arrow should rotate

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -79,11 +79,12 @@ export function SidebarNavigation() {
             ))}
           </ul>
           <ul className={styles.list}>
-            <MenuItemButton
+            <MenuItemLink
               text="Support"
               iconSrc="/icons/support.svg"
               isCollapsed={isSidebarCollapsed}
-              onClick={() => alert("Support")}
+              href="mailto:profysupport@prolog-app.com?subject=Support%20Request%20:&body=message%20goes%20here"
+              isActive={false}
             />
             <MenuItemButton
               text="Collapse"

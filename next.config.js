fde4ced3612029a2109f9aff426e393144719123
Shayne Fawcett <shayne.fawcett@gmail.com>
2024-01-24 16:14:43 -0500

    next.config fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { version } = require('./package.json');
+const { version } = require("./package.json");
 
 const nextConfig = {
   reactStrictMode: true,
@@ -10,9 +10,8 @@ const nextConfig = {
   },
 
   env: {
-    appVersion: version
-  }
-
+    appVersion: version,
+  },
 };
 
 module.exports = nextConfig;

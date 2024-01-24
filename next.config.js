52d54e155696fab46519f24ac7dcb09b5e3f9ba3
Shayne Fawcett <shayne.fawcett@gmail.com>
2024-01-24 09:59:52 -0500

    more code cleanup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { version } = require('./package.json')
+const { version } = require('./package.json');
 
 const nextConfig = {
   reactStrictMode: true,
